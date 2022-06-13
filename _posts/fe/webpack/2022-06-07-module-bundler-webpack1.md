4ab9c8c87e11acf3c7ce4cfc89dfa954f25cfe7a
wjddk0909 <fjddkcb@naver.com>
2022-06-13 22:35:58 +0900

    webpack Nodj.js와 NPM

--- _posts/fe/webpack/2022-06-07-module-bundler-webpack1.md
+++ _posts/fe/webpack/2022-06-07-module-bundler-webpack1.md
@@ -21,4 +21,15 @@ toc: true
 
 [수업교안사이트](https://joshua1988.github.io/webpack-guide/)
 
+## 1.1 npm 초기화 명령어 - init
+
+- npm init 으로 package.json을 생성
+- 또는 npm init -y 로 자동 생성
+
+## 1.2 npm 설치 명령어 - install
+
+- npm install jquery 로 설치하면 node_modules폴더 생성되고 그 안에 라이브러리가 설치됨
+- packge.json에도 jquery의 버전도 명시 되어 있음
+
+## npm을 사용하는 이유와 첫 번째 장점
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -498,7 +498,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Node.js와 NPM",
-        "excerpt":"개발 환경 구성      Chrome   Git   Visual Studio Code   Node.js(v12.x 이상)   단축키  명령어 입력 : command + shift + p   수업교안사이트   ","categories": ["webpack"],
+        "excerpt":"개발 환경 구성      Chrome   Git   Visual Studio Code   Node.js(v12.x 이상)   단축키  명령어 입력 : command + shift + p   수업교안사이트   1.1 npm 초기화 명령어 - init      npm init 으로 package.json을 생성   또는 npm init -y 로 자동 생성   1.2 npm 설치 명령어 - install      npm install jquery 로 설치하면 node_modules폴더 생성되고 그 안에 라이브러리가 설치됨   packge.json에도 jquery의 버전도 명시 되어 있음   npm을 사용하는 이유와 첫 번째 장점   ","categories": ["webpack"],
         "tags": ["node.js","npm"],
         "url": "/webpack/module-bundler-webpack1/",
         "teaser": null
